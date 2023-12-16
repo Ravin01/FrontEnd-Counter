9a97dd669c6638681339deefd2353c77ed1fdfcb
Ravin G <100956485+Ravin01@users.noreply.github.com>
2023-12-16 20:08:54 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,16 @@
+"use strict";
+var countDownDate = new Date("jan 10,2024 10:00:00").getTime();
+var x = setInterval(function(){
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000*60*60*24));
+    var hours = Math.floor((distance / (1000*60*60)) % 24);
+    var minutes = Math.floor((distance / 1000 / 60) % 60);
+    var seconds = Math.floor((distance / 1000) % 60);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+},1000);
